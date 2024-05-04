3eac79f2939626c31a2f34e4bd523f2c68f11ad5
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-05-04 03:01:01 +0100

    Deploy_static Task.0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,8 +20,30 @@ sudo echo "<html>
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-sudo service nginx restart
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" >/etc/nginx/sites-available/default
+
+service nginx restart
