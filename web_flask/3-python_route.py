7deeaebac2fb20d72b01d4dd54ee2c162214a993
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-05-17 20:52:43 +0100

    Task.3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web app
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Print Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Print HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Print C is fun/cool """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text():
+    """ Print Python + text input """
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
