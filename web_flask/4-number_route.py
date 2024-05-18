6186e968d89ecd1712908a0e8e58f7c6ef1e599c
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-05-18 22:44:33 +0100

    Task.4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web app
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Print Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Print HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Print C is fun/cool """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ Print Python + text input """
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/python/<int:n>', strict_slashes=False)
+def python_int(n):
+    """ Print Python + int input """
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
