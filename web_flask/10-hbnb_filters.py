81eb15c052ff479e0908c65f9812cb14532c857b
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-05-20 03:36:14 +0100

    Task.11

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """
+    Displays the main HBnB filters HTML page.
+    """
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template('10-hbnb_filters.html',
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    """ Run the web app on 0.0.0.0 port 5000 """
+    app.run(host='0.0.0.0', port=5000)
